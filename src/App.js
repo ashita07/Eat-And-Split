74fb870405cd32c05e73c1d88a89cb7b8ef4f936
Ashita <ashitam2407@gmail.com>
2024-09-08 21:35:22 +0530

    added friends list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const initialFriends = [
+  {
+    id: 112123,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+        <AddFriend />
+        <Button>Add Friend</Button>
+      </div>
     </div>
   );
 }
+function FriendList() {
+  const friend = initialFriends;
+  return (
+    <ul>
+      {friend.map((f) => (
+        <Friend friend={f} key={f.id} />
+      ))}
+    </ul>
+  );
+}
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance < 0 && (
+        <p className="red">
+          you owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p>You and your friend {friend.name} are even</p>
+      )}
+      <button className="button">Select</button>
+    </li>
+  );
+}
 
-export default App;
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function AddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🙃Friend</label>
+      <input type="text" />
+      <label>🙃Image url</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
